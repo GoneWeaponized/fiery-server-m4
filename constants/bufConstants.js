0d8bb4e4e37e24f362aaf1cc7d46a139a47e834b
Simpcast Productions <66287897+RuiKurumi@users.noreply.github.com>
2026-07-23 03:17:26 +0800

    Update bufConstants.js

--- constants/bufConstants.js
+++ constants/bufConstants.js
@@ -1,8 +1,8 @@
 const ERROR_MSGS = {
-    const GENERIC_ERROR_MSG: 6,
-    const ERROR_NAME: 12,
-    const ERROR_WEIRD: 18,
-    const ERROR_MISMATCH: 24
+    GENERIC_ERROR_MSG: 6,
+    ERROR_NAME: 12,
+    ERROR_WEIRD: 18,
+    ERROR_MISMATCH: 24
 }
 
-module.exports = {ERROR_MSGS;
+module.exports = {ERROR_MSGS};
