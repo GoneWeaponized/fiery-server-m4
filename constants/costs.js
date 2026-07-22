e0833681e427f07c4ad80d5b2456d33d005ea9d6
Simpcast Productions <66287897+RuiKurumi@users.noreply.github.com>
2026-07-23 03:20:23 +0800

    Update costs.js
    Added new buildables to cost object and lookup constant

--- constants/costs.js
+++ constants/costs.js
@@ -1,11 +1,17 @@
 const COSTS = Object.freeze({
     BASE: 30000,
-    SAMSITE: 10000
+    SAMSITE: 10000,
+    CIWS: 5000,
+    ARTILLERY: 5000,
+    AIRBASE: 100000
 });
 
 const COST_LOOKUP = [
     COSTS.BASE,    // Index 0
-    COSTS.SAMSITE  // Index 1
+    COSTS.SAMSITE,  // Index 1
+    COSTS.CIWS,
+    COSTS.ARTILLERY,
+    COSTS.AIRBASE
 ];
 
 module.exports = { COSTS, COST_LOOKUP };
