b789d6a0416d99831d379630a56c36525f15d8b1
Simpcast Productions <66287897+RuiKurumi@users.noreply.github.com>
2026-07-23 22:53:36 +0800

    Update structureHandler.js

--- staff/structureHandler.js
+++ staff/structureHandler.js
@@ -1,1 +1,16 @@
 // structureHandler code
+
+const structures = require('../data/structures.json');
+const { saveStructures } = require('./buildingHandler');
+
+for (const structure of structures){
+    if (structure.data.isOnline)
+        continue;
+
+    if(Date.now() - structure.data.bootStarted >= structure.data.bootTime){
+        structure.data.isOnline = true;
+        console.log(`${structure.name} is now online`);
+
+        saveStructures();
+    }
+}
