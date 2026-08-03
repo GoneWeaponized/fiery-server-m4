7b0886fc0221cbb2324384667e2f17518b839a00
Simpcast Productions <66287897+RuiKurumi@users.noreply.github.com>
2026-08-03 22:23:01 +0800

    slight patch

--- staff/buildingHandler.js
+++ staff/buildingHandler.js
@@ -2,18 +2,18 @@ const krakey = require('./krakey');
 const path = require("path");
 const resourceHandler = require('./resourceHandler');
 const fs = require('fs');
-const STRUCTURES_FILE = path.join(__dirname,"../data/structures.json");
+const STRUCTURES_FILE = path.join(__dirname, "../data/structures.json");
 const crypto = require("crypto");
-const { BUILDABLES, BUILDABLES_BY_ID } = require("../classes/buildableTypes");
+const { BUILDABLES_BY_ID } = require("../classes/buildableTypes");
 
 let structures = [];
 
-if (fs.existsSync(STRUCTURES_FILE)){
-    structures = JSON.parse(fs.readFileSync(STRUCTURES_FILE,"utf8"));
+if (fs.existsSync(STRUCTURES_FILE)) {
+    structures = JSON.parse(fs.readFileSync(STRUCTURES_FILE, "utf8"));
 }
 
-function saveStructures(){
-    fs.writeFileSync(STRUCTURES_FILE,JSON.stringify(structures,null,2));
+function saveStructures() {
+    fs.writeFileSync(STRUCTURES_FILE, JSON.stringify(structures, null, 2));
 }
 
 function construct(socket, lat, long, typeId, uuid) {
@@ -25,6 +25,7 @@ function construct(socket, lat, long, typeId, uuid) {
         return false;
     }
 
+    // Validate and deduct the player's resources once.
     if (!validateRequest(socket, uuid, buildable.cost)) {
         return false;
     }
@@ -36,17 +37,17 @@ function construct(socket, lat, long, typeId, uuid) {
     return true;
 }
 
-function addStructure(structure, uuid){
+function addStructure(structure) {
     structures.push(structure);
-    console.log(structure + " -> Has been added.");
+    console.log(`${structure.name} -> Has been added.`);
 }
 
 function validateRequest(socket, uuid, cost) {
 
     const error = Buffer.alloc(3);
 
-    error.writeUInt16BE(3,0);
-    error.writeUInt8(18,2);
+    error.writeUInt16BE(3, 0);
+    error.writeUInt8(18, 2);
 
     if (!resourceHandler.deductResource(uuid, cost)) {
 
@@ -60,11 +61,10 @@ function validateRequest(socket, uuid, cost) {
     return true;
 }
 
-function addBuilding(socket, uuid, building, buildable) {
-    if (!validateRequest(socket, uuid, buildable.cost)) {
-        return;
-    }
+function addBuilding(building, buildable) {
+
     const subId = crypto.randomBytes(8).toString('hex');
+
     const structure = {
         owner: building.owner,
         type: buildable.typeId,
@@ -75,18 +75,23 @@ function addBuilding(socket, uuid, building, buildable) {
         },
         data: {
             hp: building.Hp,
-            subId: subId,
+            subId,
             hasInventory: building.HasInv,
             isOnline: building.online,
-            bootTime: 1800000, // same for all
+            bootTime: 1800000,
             bootStarted: Date.now()
         }
-    }
+    };
+
     addStructure(structure);
     saveStructures();
 }
+
 module.exports = {
-    addBuilding, construct, saveStructures
+    addBuilding,
+    construct,
+    saveStructures
 };
-// A structure inventory file will be created separately for structures that do have a inventory
+
+// A structure inventory file will be created separately for structures that do have an inventory.
 // Running outta time bruh
